ca60ee39f72b9516b04c13252be67ca69ae7feec
sergey-emelyanov <emelia35@yandex.ru>
2023-01-07 09:08:31 +0300

    make some changes

--- gendiff/test_for_test.py
+++ gendiff/test_for_test.py
@@ -1,5 +1,5 @@
 from gendiff import generate_diff
 
-diff1 = generate_diff('D:\pythonProject50\python-project-50\\tests\\fixtures\\file1.json',
-                      'D:\pythonProject50\python-project-50\\tests\\fixtures\\file2.json')
+diff1 = generate_diff(r'D:\pythonProject50\python-project-50\\tests\\fixtures\\file1.json',
+                      r'D:\pythonProject50\python-project-50\\tests\\fixtures\\file2.json')
 print(diff1)
